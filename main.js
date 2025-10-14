5286724a5cf82ef51f1883efaccb03d6e37ffb02
Azis Panji <azis92@gmail.com>
2025-10-14 10:56:09 +0700

    emot baru

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 function startGame(game) {
   window.location.href = `game/${perasaan - hari - ini}/${perasaan.split('-')[0]}.html`;
 }
+// script.js
+
+document.addEventListener('DOMContentLoaded', function () {
+  const links = document.querySelectorAll('.game-link');
+
+  links.forEach((link) => {
+    link.addEventListener('click', function (event) {
+      event.preventDefault(); // supaya nggak pindah halaman
+      const gameName = this.getAttribute('data-game');
+      alert('ini nanti ya bu guruuu, programmer nya mager plus banyak tugas hahaha ');
+    });
+  });
+});
